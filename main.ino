e06976ddff98c66d00cd37e4220c6a98172a241f
nextrix <110208771+NextrixVFX@users.noreply.github.com>
2024-11-05 05:36:49 -0600

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,4 +1,3 @@
-#include <SPI.h>
 #include <RF24.h>
 #include <nRF24L01.h>
 
@@ -7,22 +6,12 @@
 #define CSN 0
 #define CE 2
 
-RF24 radio(2, 0);  // First transmitter on pins CE = 2, CSN = 0
-// RF24 radio2(4, 16);  // Second transmitter on pins CE = 4, CSN = 16
+RF24 radio(CE, CSN);
 
 const char payload[] = "00000000000";
 const uint8_t MAX_CHANNELS = 85;
 const uint8_t MIN_CHANNELS = 0;
 uint32_t amnt = 0 % 2;
-
-uint8_t writeRegister(uint8_t reg, uint8_t value){
-  uint8_t status;
-  digitalWrite(CSN, LOW);
-  status = SPI.transfer( W_REGISTER | ( REGISTER_MASK & reg ) );
-  SPI.transfer(value);
-  digitalWrite(CSN, HIGH);
-  return status;
-}
 
 void setup() {
   Serial.begin(115200);
@@ -46,14 +35,6 @@ void setup() {
     module.setPALevel(RF24_PA_MAX);
     module.setDataRate(RF24_2MBPS);
   }
-
-  SPI.begin();
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setClockDivider(SPI_CLOCK_DIV2);
-  SPI.setBitOrder(MSBFIRST);
-
-  writeRegister(EN_AA, 0x0);
-  writeRegister(RF_SETUP, 0x0F);
 }
 
 void loop() {
